d7dcb04882ada0ef9057bc77296aa7129b7ec6e9
gekony <118277763+gekony@users.noreply.github.com>
2025-07-16 20:18:11 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Pythonパッケージのビルドに必要なシステムライブラリをインストール
+apt-get update && apt-get install -y build-essential
+
+# Pythonライブラリをインストール
 pip install -r requirements.txt
